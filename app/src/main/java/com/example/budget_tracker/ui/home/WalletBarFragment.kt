6c1c76a817a8f92bbb8ae7002058c3b9bd03d591
nikica <nikicaraznatovic123@gmail.com>
2023-05-27 19:38:49 +0200

    first commit

---
+++ app/src/main/java/com/example/budget_tracker/ui/home/WalletBarFragment.kt
@@ -0,0 +1,18 @@
+package com.example.budget_tracker.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.budget_tracker.R
+
+class WalletBarFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wallet_bar, container, false)
+    }
+}
